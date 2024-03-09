53f92fca5f13fc4f10c4a1f9747aec6f39f6b0f4
Ágatha <agathamagnusg@hotmail.com>
2024-03-09 16:52:35 -0300

    ✨ Add create method to BookService
    Implementa funcionalidade de adição de livros na classe BookService. Verifica a existência de um livro pelo código de barras antes de inserir. Caso o livro já exista, uma exceção é lançada, evitando duplicatas no banco de dados.

--- src/modules/book/book.service.ts
+++ src/modules/book/book.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { BookDto } from './book.dto';
+import { PrismaService } from 'src/database/PrismaService';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: BookDto) {
+    const bookExists = await this.prisma.book.findFirst({
+      where: {
+        bar_code: data.bar_code,
+      },
+    });
+    if (bookExists) {
+      throw new Error('Book already exists');
+    }
+    const book = await this.prisma.book.create({
+      data,
+    });
+    return book;
+  }
+}

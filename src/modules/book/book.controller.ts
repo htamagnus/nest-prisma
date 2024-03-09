5ed9437ddfdd1c6cb45eec453c9e2a165bd7716c
Ágatha <agathamagnusg@hotmail.com>
2024-03-09 16:56:34 -0300

    ✨ Add create method to BookController
    Implementa endpoint de criação de livros no BookController, utilizando o método create do BookService para inserir novos registros, com validação dos dados de entrada através do BookDto

--- src/modules/book/book.controller.ts
+++ src/modules/book/book.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { BookService } from './book.service';
+import { BookDto } from './book.dto';
 
 @Controller('book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
+
+  @Post()
+  async create(@Body() data: BookDto) {
+    return this.bookService.create(data);
+  }
 }
